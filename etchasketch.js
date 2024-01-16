4c2d6af1eb591a6582232d1b7b1f2c90a5926ad6
Edwing Muñoz <eamc201168@gmail.com>
2024-01-15 23:57:38 -0500

    add function to prompt the user after click, adjust first drafts for functions to draw the grid

--- etchasketch.js
+++ etchasketch.js
@@ -1,5 +1,24 @@
 const container = document.querySelector('.gridContainer');
+const squares = document.querySelector('.squares');
 
+squares.addEventListener('click', howManySquares);
+gridCreation();
+
+function howManySquares() {
+    let cuadros = Number(prompt("How many squares per side do you want to display?"));
+    gridUpdate(cuadros);
+}
+
+function gridUpdate(sqrs) {
+    container.innerHTML = '';
+    for (let j = 1; j <= sqrs; j++) {
+        for (let i = 1; i <= sqrs; i++) {
+            const newDiv = document.createElement('div');
+            /* newDiv.textContent = i; */
+            container.append(newDiv);
+        }
+    }
+}
 function gridCreation() {
     for (let j = 1; j <= 16; j++) {
         for (let i = 1; i <= 16; i++) {
@@ -9,5 +28,3 @@ function gridCreation() {
         }
     }
 }
-
-gridCreation();
